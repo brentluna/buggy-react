37eea86681321d4419aa9aca295a28bf9839015d
Brent Luna <brentadamluna@gmail.com>
2019-01-21 16:20:51 -0800

    branch that works

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,37 +1,45 @@
 import React from 'react';
-import ListItem from './ListItem'
+import ListItem from './ListItem';
 
 class List extends React.Component {
   constructor(props) {
+    super(props);
     this.state = {
       count: 0,
       searchVal: ''
-    }
+    };
+    this.handleInputChanges = this.handleInputChanges.bind(this);
   }
 
   renderItems() {
-    const {count} = this.state;
+    const { count } = this.state;
     const elements = [];
-    for(let i = 0; i < 20; i++) {
-      elements.push(
-        <ListItem foo={i > count ? 'bar' : 'baz'} />
-      )
+    for (let i = 0; i < 20; i++) {
+      elements.push(<ListItem foo={i > count ? 'bar' : 'baz'} />);
     }
     return elements;
   }
 
   handleInputChanges(value) {
-    this.setState({searchVal: value})}
+    this.setState({ searchVal: value });
   }
 
   render() {
-    const {count} = this.state;
+    const { count } = this.state;
     return (
       <div>
-        <input type='text' onClick={this.handleInputChanges}/>
-        <div onClick={this.setState({count: count + 1})}>Pump it UP</div>
-        {this.renderItems}
+        <input type="text" onClick={this.handleInputChanges} />
+        <div
+          className='pump-it-button'
+          onClick={() => this.setState({ count: count + 1 })}
+        >
+          Pump it UP
+        </div>
+        <hr/>
+        {this.renderItems()}
       </div>
-    )
+    );
   }
 }
+
+export default List;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 
 class ListItem extends React.Component {
   constructor(props){
+    super(props)
     this.state = {
       givenFoo: this.props.foo
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    this.setState({givenFoo: this.props.foo})
+    if(prevState.givenFoo !== this.props.foo) {
+      this.setState({givenFoo: this.props.foo})
+    }
   }
 
   render() {
     const {givenFoo} = this.state;
     return (
       <div>
-        Given Foo:
-      </div>
-      <div>
-        {givenFoo}
+        <div>
+          Given Foo:
+        </div>
+        <div>
+          {givenFoo}
+        </div>
       </div>
     )
   }
 }
 
-export ListItem;
+export default ListItem;
