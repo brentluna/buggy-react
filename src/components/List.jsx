0247015a6ca17cb46e0c7d55ac1df6e1b8bbc44c
Brent Luna <brentadamluna@gmail.com>
2019-01-30 14:14:02 -0800

    Hari's work

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import ListItem from './ListItem'
+import ListItem from './ListItem';
 
 class List extends React.Component {
+
   constructor(props) {
+    super(props);
     this.state = {
       count: 0,
       searchVal: ''
     }
+    this.handleInputChanges = this.handleInputChanges.bind(this);
   }
 
   renderItems() {
     const {count} = this.state;
     const elements = [];
-    for(let i = 0; i < 20; i++) {
+    for(let i = 0; i < 2; i++) {
       elements.push(
         <ListItem foo={i > count ? 'bar' : 'baz'} />
       )
@@ -20,27 +23,31 @@ class List extends React.Component {
     return elements;
   }
 
+  onClickFun() {
+
+    if (this.state .searchVal === 'Yes') {
+              this.setState({count: this.state.count + 1})
+              this.renderItems();
+            }
+  }
+
   handleInputChanges(value) {
-    this.setState({searchVal: value})}
+    this.setState({searchVal: value})
   }
 
   render() {
     const {count} = this.state;
     return (
       <div>
-        <input type='text' placeHolder={'Type "Yes" to enable button'}onClick={this.handleInputChanges} style={{width: '300px'}}/>
-        <div class='pump-it-button'
-          onClick={
-            if (searchVal === 'Yes') {
-              this.setState({count: count + 1})
-            }
-          }
-        >
+        <input type='text' placeHolder={'Type "Yes" to enable button'} onClick={this.handleInputChanges} style={{width: '300px'}}/>
+        <div class='pump-it-button' onClick={this.onClickFun()}>
           Pump it UP
         </div>
         <hr/>
-        {this.renderItems}
+        {this.renderItems()}
       </div>
-    )
+    );
   }
 }
+
+export default List;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 
 class ListItem extends React.Component {
   constructor(props){
+    super(props);
     this.state = {
       givenFoo: this.props.foo
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    this.setState({givenFoo: this.props.foo})
+    // this.setState({givenFoo: this.props.foo})
   }
 
   render() {
     const {givenFoo} = this.state;
     return (
       <div>
-        {Given Foo: givenFoo}
+        Given Foo: {this.state.givenFoo}
+        <hr/>
       </div>
-      <hr/>
     )
   }
 }
 
-export ListItem;
+export default ListItem;
